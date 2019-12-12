8a1b34c25a7dd75fb14c27e099ab1f4416c2733b
Stevan.Cunningham <stevan.cunningham@ticketmaster.com>
2019-12-11 17:30:16 -0700

    second commit

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/go-kit/kit/log"
+	"github.com/go-kit/kit/endpoint"
+	httptransport "github.com/go-kit/kit/transport/http"
+)
+
+func main() {
+	logger := log.NewLogfmtLogger(os.Stderr)
+
+	svc := stringService{}
+
+	var uppercase endpoint.Endpoint
+	uppercase = makeUpperCaseEndpont(svc)
+	uppercase = loggingMiddleware(log.With(logger, "method", "uppercase"))(uppercase)
+
+	var count endpoint.Endpoint
+	count = makeCountEndpoint(svc)
+	count = loggingMiddleware(log.With(logger, "method", "count"))(count)
+
+	uppercaseHandler := httptransport.NewServer(
+		makeUpperCaseEndpont(svc),
+		decodeUppercaseRequest,
+		encodeResponse,
+	)
+
+	countHandler := httptransport.NewServer(
+		makeCountEndpoint(svc),
+		decodeCountRequest,
+		encodeResponse,
+	)
+
+	http.Handle("/uppercase", uppercaseHandler)
+	http.Handle("/count", countHandler)
+}
